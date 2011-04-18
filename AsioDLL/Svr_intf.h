b24a8ede082b7d971b1e186dea244ae88771a1eb
mmzmagic <mmzmagic@gmail.com>
2011-04-18 07:12:01 +0000

    建立svn 版本v1.0.1

---
+++ AsioDLL/Svr_intf.h
@@ -0,0 +1,9 @@
+// Svr_intf.h : main header file for the Svr_intf DLL
+//
+
+#pragma once
+
+
+#include "resource.h"		// main symbols
+
+
---
+++ AsioDLL/stdafx.cpp
@@ -0,0 +1,6 @@
+// stdafx.cpp : source file that includes just the standard includes
+// Svr_intf.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+#include <windows.h>
